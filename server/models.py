d3662fbfaa5cacef8afe5e221202c8be516df823
Monica Garcia <monicairenegarcia@gmail.com>
2024-07-10 10:53:40 -0700

    update models

--- server/models.py
+++ server/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.hybrid import hybrid_property
 class Student(db.Model, SerializerMixin):
     __tablename__ = "students"
 
-    serialize_rules = ('-accommodations.students', '-user.students', '-classes.student')
+    serialize_rules = ('-accommodations.student', '-user.students')
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
@@ -13,17 +13,20 @@ class Student(db.Model, SerializerMixin):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     user = db.relationship('User', back_populates='students')
-    
-    classes = db.relationship('Classes', back_populates='student')##add cascade=all and delete-orphan
+    accommodations = db.relationship('Accommodation', back_populates='student')
 
 class Accommodation(db.Model, SerializerMixin):
     __tablename__ = "accommodations"
 
+    serialize_rules = ('-comment.accommodations', '-student.accommodations', '-category.accommodations')
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
 
-    
-    
+    comment = db.relationship('Comment', back_populates='accommodations')##add cascade=all and delete
+    student = db.relationship('Student', back_populates='accommodations')
+    category = db.relationship('Category', back_populates='accommodations')
+
 class User(db.Model, SerializerMixin):
     __tablename__ = "users"
     
@@ -49,11 +52,21 @@ class User(db.Model, SerializerMixin):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
     
-class Classes(db.Model, SerializerMixin):
-    __tablename__ = "classes"
+class Category(db.Model, SerializerMixin):
+    __tablename__ = "categories"
 
+    serialize_rules = ('-accommodations.category',)
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+    description = db.Column(db.String)
 
-    student = db.relationship('Student', back_populates='classes')
+    accommodations = db.relationship("Accommodation", back_populates="category")
+    
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    serialize_rules = ('-accommodations.comment',)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String)
+    accommodations_id = db.Column(db.Integer, db.ForeignKey("accommodations.id"))
+
+    accommodations = db.relationship("Accommodation", back_populates="comment")
