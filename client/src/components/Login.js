7c12dc7d3e0deab1358e0f7fcca5b5d41e806dfb
Monica Garcia <monicairenegarcia@gmail.com>
2024-10-06 14:44:32 -0700

    add conditional error message to login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import SignUp from './SignUp'
 import { useFormik } from "formik";
+import * as yup from "yup";
 function Login({ onLogin }) {
     
     const [showComponent, setShowComponent] = useState(false);
+    const [errorLoggingIn, setErrorLoggingIn] = useState(false);
     const navigate = useNavigate()
+
+    const formSchema = yup.object().shape({
+        username: yup
+        .string()
+        .required("username is required"),
+        password: yup
+        .string()
+        .required("password is required"),
+      });
 
     const formik = useFormik({
         initialValues: {
@@ -26,7 +37,7 @@ function Login({ onLogin }) {
                 res.json().then(user => onLogin(user))
             }
             else {
-                throw new Error('Error fetching student data');
+                setErrorLoggingIn(true);
             }
         })
         .catch(error => {
@@ -34,7 +45,7 @@ function Login({ onLogin }) {
     }
 })
     function handleClick() {
-        setShowComponent(true); // Set showComponent to true when the button is clicked
+        setShowComponent(true);
     }
 
     return (
@@ -67,6 +78,9 @@ function Login({ onLogin }) {
                         <button type="submit" className="border-slate-200 bg-slate-500 text-white text-xl font-semibold">SUBMIT</button>
                     </div>
                 </form>
+                {errorLoggingIn ? (
+                <div className="text-3xl italic py-5 text-red-700">Username or password is incorrect, please try again.</div>
+            ) : null}
                 <div className="bg-slate-200 border-2 border-slate-700">    
                     <p className="italic text-3xl py-5 text-slate-700">Or, if you don't have an account...</p>
                     <button onClick={handleClick} className="border-slate-500 text-xl font-semibold">SIGN UP</button>
