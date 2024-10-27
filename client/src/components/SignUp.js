f49da6f3f1e58c120889a10cde16adad93bf3a2a
Monica Garcia <monicairenegarcia@gmail.com>
2024-10-27 16:41:54 -0700

    change fetch request to deployed url

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -42,7 +42,7 @@ function SignUp({ onLogin }) {
         },
         validationSchema: formSchema,
         onSubmit: (values) => {
-          fetch("http://127.0.0.1:5000/signup", {
+          fetch("https://eduplan.onrender.com/signup", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
