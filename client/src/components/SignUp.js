4da503c10d60732ac7f605b28d68e8241cc41c4f
Monica Garcia <monicairenegarcia@gmail.com>
2024-10-27 16:34:29 -0700

    fix endpoint port

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -42,7 +42,7 @@ function SignUp({ onLogin }) {
         },
         validationSchema: formSchema,
         onSubmit: (values) => {
-          fetch("http://127.0.0.1:5555/signup", {
+          fetch("http://127.0.0.1:5000/signup", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
